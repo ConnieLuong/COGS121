1907b8ecae5214c08f8e25bd2e089b7fa0ee9425
Connie Luong <col032@ucsd.edu>
2019-04-29 16:07:23 -0700

    index and server setup WIP

---
+++ secret.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyD0R6vfBJHgyT5pkOJZKIQuPN-A0ybDfz4",
+    authDomain: "poppa-hub.firebaseapp.com",
+    databaseURL: "https://poppa-hub.firebaseio.com",
+    projectId: "poppa-hub",
+    storageBucket: "poppa-hub.appspot.com",
+    messagingSenderId: "907928434472"
+  };
+  firebase.initializeApp(config);
--- server.js
+++ server.js
@@ -1,18 +1,11 @@
 const express = require("express");
 const app = express();
 app.use(express.static("static_files"));
+//import secret.js
+
 
 app.listen(3000, () => {
   console.log("Server started at http://localhost:3000/");
 });
 
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyD0R6vfBJHgyT5pkOJZKIQuPN-A0ybDfz4",
-  authDomain: "poppa-hub.firebaseapp.com",
-  databaseURL: "https://poppa-hub.firebaseio.com",
-  projectId: "poppa-hub",
-  storageBucket: "poppa-hub.appspot.com",
-  messagingSenderId: "907928434472"
-};
-firebase.initializeApp(config);
+
