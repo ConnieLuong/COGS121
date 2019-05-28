9913ec95e0d5c3c916dfc877bf128374c34fd6b1
Connie Luong <col032@ucsd.edu>
2019-05-27 22:21:59 -0700

    added searchTIps function

--- static_files/js/search.js
+++ static_files/js/search.js
@@ -13,13 +13,51 @@ function search(query){
 }
 
 function searchTips(query){
+    var res = [];
+    $.ajax({
+        url: 'tips',
+        type: 'GET',
+        data: JSON,
+        success: function (data) {
+            var tips = data; //{tip0:{tip_tags: [], tip_text:'', tip_title:''}...}
+            
+            //go through each tip and add it to res if any of tip_tags, tip_text, tip_title contains the query
+            //tipValue = {tip_img:'', tip_num:'', tip_tags: [], tip_text:'', tip_title:''}
+            _.each(tips, function(tipValue){
+                _.chain(tipValue).omit(function(value, key, object) {
+                    return (key=='tip_img' || key=='tip_num');
+                })// {tip_tags: [], tip_text:'', tip_title:''}
+                .values() // [[''], '', '']
+                .flatten()// ['','','']
+                .each(function(value, key){
+                    if(value.includes(query)){
+                        res.push(key);
+                    }
+                })
+                .value(); // [tip_tags, tip_text, tip_title]
+            });
+        }
+    });
 
-}
-
-function searchSongs(query){
-
+    return _.uniq(res);
 }
 
 function searchStories(query){
-    
+    $.ajax({
+        url: 'stories',
+        type: 'GET',
+        data: JSON,
+        success: function (data) {
+        }
+    });
+}
+
+function searchSongs(query){
+    $.ajax({
+        url: 'songs',
+        type: 'GET',
+        data: JSON,
+        success: function (data) {
+        }
+    });
 }
