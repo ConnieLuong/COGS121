37e959d8ecf54e4d8760a798bdae81fa70688203
Connie Luong <connie@Connies-iMac.local>
2019-05-29 10:59:24 -0700

    fixed ajax call favorite bug

--- static_files/js/search.js
+++ static_files/js/search.js
@@ -39,7 +39,7 @@ $(document).ready(function () {
 
             //check if tip is favorited
             $.ajax({
-                url: 'favorite',
+                url: 'getFravorite',
                 type: 'POST',
                 data: { collection: "tips" },
                 success: function (data) {
