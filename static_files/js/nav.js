c2d9478be29d67dfb7ac3df05bee5b3a424c31e6
Hao-In Choi <hic003@ucsd.edu>
2019-06-03 09:37:52 -0700

    headers

--- static_files/js/nav.js
+++ static_files/js/nav.js
@@ -1,71 +1,71 @@
 /**
  * File: nav.js
  * Description: logic for the navbar & signin/signout button
- * Authors: Boya Ren, Connie Luong
+ * Authors: Boya Ren, Connie Luong, Hao-In Choi
  */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "250px";
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.width = "0";
 }
 
-$(document).ready(function () {
-    //update navbar on load to show signin or signout button
+$(document).ready(function() {
+  //update navbar on load to show signin or signout button
+  $.ajax({
+    url: "getUser",
+    type: "GET",
+    data: JSON,
+    success: data => {
+      if (data.text == "Sign out") {
+        $(".signinNav").html(data.text);
+        $(".signinNav").addClass("signoutNav");
+        $(".signinNav").attr("href", "#");
+        $("#greeting").html("Hey, <br>" + data.displayName + "!");
+      } else if (data.text == "Sign in") {
+        $(".signinNav").html(data.text);
+        $(".signinNav").removeClass("signoutNav");
+        $(".signinNav").attr("href", "./signin.html");
+        $("#greeting").html("Hi there!");
+      }
+    },
+    error: data => {
+      console.log("Error from get request to isUserSignedin");
+    }
+  });
+
+  //sign out
+  $(document).on("click", ".signoutNav", function(event) {
+    console.log(event.target.classList);
     $.ajax({
-        url: 'getUser',
-        type: 'GET', 
-        data: JSON,
-        success: (data) => {
-            if(data.text=="Sign out"){
-                $('.signinNav').html(data.text);
-                $('.signinNav').addClass('signoutNav');
-                $('.signinNav').attr('href', '#');
-                $('#greeting').html("Hey, <br>"+data.displayName+"!");
-            }else if (data.text == "Sign in"){
-                $('.signinNav').html(data.text);
-                $('.signinNav').removeClass('signoutNav');
-                $('.signinNav').attr('href', './signin.html');
-                $('#greeting').html("Hi there!");
-            }
-        },
-        error: (data) => {
-            console.log("Error from get request to isUserSignedin");
-        }
+      url: "signOut",
+      type: "GET",
+      data: JSON,
+      success: data => {
+        console.log("successful sign out");
+        location.href = "./signin.html";
+      }
     });
+  });
 
-    //sign out
-    $(document).on('click', '.signoutNav', function(event){
-        console.log(event.target.classList);
-        $.ajax({
-            url: 'signOut',
-            type: 'GET', 
-            data: JSON,
-            success: (data) => {
-                console.log("successful sign out");
-                location.href = "./signin.html";
-            }
-        });
+  //logo fade out/fade in when search bar expands
+  $(document).on("click", "#query", function(event) {
+    $("#logo").fadeOut(150);
+  });
+  $(document).on("click", "body", function(event) {
+    if (event.target.id != "query") {
+      $("#logo")
+        .delay(500)
+        .fadeIn();
+    }
+  });
 
-
-    });
-
-    //logo fade out/fade in when search bar expands
-    $(document).on('click', '#query', function(event){
-        $('#logo').fadeOut(150);
-    });
-    $(document).on('click', 'body', function(event){
-        if(event.target.id != 'query'){
-            $('#logo').delay(500).fadeIn();
-        }
-    });
-
-    //search
-    $("form#search-bar").on("submit", function(e) {
-        e.preventDefault(); //prevents refresh
-        var query = $('#query').val(); 
-        localStorage.setItem("query",query); //store query so can use when go to searchResults page
-        location.href = "./searchResults.html"; //redirect to search results page
-    });
+  //search
+  $("form#search-bar").on("submit", function(e) {
+    e.preventDefault(); //prevents refresh
+    var query = $("#query").val();
+    localStorage.setItem("query", query); //store query so can use when go to searchResults page
+    location.href = "./searchResults.html"; //redirect to search results page
+  });
 });
